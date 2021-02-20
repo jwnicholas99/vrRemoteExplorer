28c4e896385d7c88963b401ce3bbfc621c0bfee8
jwnicholas99 <jwnicholas99@gmail.com>
2021-02-20 11:44:31 -0500

    [FEAT] Add Player to scene
    * Replace CameraRig with Player so that hands can be seen.
    * Add ShowController script so that player hands can be seen with
      controller in hand.
    * Add PlayerController script so that player can move with trackpad.

---
+++ Assets/Scripts/ShowController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ShowController : MonoBehaviour
+{
+    public bool isShowController = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var hand in Player.instance.hands){
+            if (isShowController){
+                hand.ShowController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
+            } else{
+                hand.HideController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithoutController);
+            }
+        }
+    }
+}
