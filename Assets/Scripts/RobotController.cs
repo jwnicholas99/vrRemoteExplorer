ff4a096d1dd0bcd52c995414f6ca8925fbc8eb9f
jwnicholas99 <jwnicholas99@gmail.com>
2021-02-22 16:35:57 -0500

    [FEAT] Implement Robot Controller script to publish Joy messages to control robot in

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace RosSharp.RosBridgeClient {
+    public class RobotController : UnityPublisher<MessageTypes.Sensor.Joy> { 
+        public SteamVR_Action_Vector2 input;
+        public float speed = 1;
+
+        public string FrameId = "Unity";
+
+        private MessageTypes.Sensor.Joy message;
+
+        protected override void Start() {
+            base.Start();
+            InitializeMessage();
+        }
+
+        private void Update() {
+            UpdateMessage();
+        }
+
+        private void InitializeMessage() {
+            message = new MessageTypes.Sensor.Joy();
+            message.header.frame_id = FrameId;
+            message.axes = new float[2];
+            message.buttons = new int[0];
+        }
+
+        private void UpdateMessage() {
+            message.header.Update();
+
+            message.axes[0] = input.axis.x;
+            message.axes[1] = input.axis.y;
+
+            Publish(message);
+        }
+    }
+}
