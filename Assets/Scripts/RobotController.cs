732e8a5e245dea08031edf24f4e08cc6cf15d839
jwnicholas99 <jwnicholas99@gmail.com>
2021-02-22 18:18:57 -0500

    [FEAT] Implement rotation for robot

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
@@ -7,6 +8,7 @@ using Valve.VR.InteractionSystem;
 namespace RosSharp.RosBridgeClient {
     public class RobotController : UnityPublisher<MessageTypes.Sensor.Joy> { 
         public SteamVR_Action_Vector2 input;
+        public SteamVR_Action_Boolean GrabPinch;
         public float speed = 1;
 
         public string FrameId = "Unity";
@@ -25,15 +27,15 @@ namespace RosSharp.RosBridgeClient {
         private void InitializeMessage() {
             message = new MessageTypes.Sensor.Joy();
             message.header.frame_id = FrameId;
-            message.axes = new float[2];
-            message.buttons = new int[0];
+            message.axes = new float[3];
+            message.buttons = new int[1];
         }
 
         private void UpdateMessage() {
             message.header.Update();
-
             message.axes[0] = input.axis.x;
             message.axes[1] = input.axis.y;
+            message.buttons[0] = Convert.ToInt32(GrabPinch.state);
 
             Publish(message);
         }
